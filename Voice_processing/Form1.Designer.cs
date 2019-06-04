499279980dff9c575b43f28102869bf1a9b3dae3
Aleksandr Gerasimchuk <aleksandr.gerasimchuk@developex.org>
2019-06-04 18:56:25 +0300

    Init commit

---
+++ Voice_processing/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Voice_processing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(365, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create DICTIONARY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 146);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(366, 180);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(52, 8);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(190, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(52, 47);
+            this.trackBar1.Minimum = -10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(120, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(178, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(27, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Voice";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Speed";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(351, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(27, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(273, 44);
+            this.trackBar2.Maximum = 5;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(67, 45);
+            this.trackBar2.TabIndex = 8;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Noise";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(220, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "threshold";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 334);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TTS ";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Voice_processing/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Voice_processing
+{
+
+
+    public partial class Form1 : Form
+    {
+
+
+        FolderBrowserDialog saveFileDialog1=new FolderBrowserDialog();
+
+        SpeechSynthesizer voice = new SpeechSynthesizer();
+        static String PATHSOUND = @"D:\Privat\Sound\";
+        static String WAVFOLDER = "WAV";
+        static String SAMPLENANE = "sample.dic";
+        static String DICNANE = "dic.dic";
+        static String NO_WORD = "0,0";
+
+
+        // Создаю колекцию настроечных параметров програмы
+        public Dictionary<string, string> DictAlphabet = new Dictionary<string, string> {
+        {"A", NO_WORD},{"B", NO_WORD},{"C", NO_WORD},{"D", NO_WORD},{"E", NO_WORD},{"F", NO_WORD},{"G", NO_WORD},
+        {"H", NO_WORD},{"I", NO_WORD},{"J", NO_WORD},{"K", NO_WORD},{"L", NO_WORD},{"M", NO_WORD},{"N", NO_WORD},
+        {"O", NO_WORD},{"P", NO_WORD},{"Q", NO_WORD},{"R", NO_WORD},{"S", NO_WORD},{"T", NO_WORD},{"U", NO_WORD},
+        {"V", NO_WORD},{"W", NO_WORD},{"X", NO_WORD},{"Y", NO_WORD},{"Z", NO_WORD},
+        };
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Get seting voices
+            foreach (InstalledVoice voice1 in voice.GetInstalledVoices())
+            {
+                VoiceInfo info = voice1.VoiceInfo;
+                this.comboBox1.Items.Add(info.Name);
+            }
+            // Set default selected voice
+            this.comboBox1.SelectedIndex = 1;
+            // Set curen position speed
+            this.textBox1.Text = this.trackBar1.Value.ToString();
+            //Show curent position noise thrushold
+            this.trackBar2.Value = 2;
+            this.textBox2.Text = this.trackBar2.Value.ToString();
+        }
+
+        // Оттображение настройки скорости речи
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.textBox1.Text = this.trackBar1.Value.ToString();
+        }
+
+
+        // Оттображение настройки шумового порога
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            this.textBox2.Text = this.trackBar2.Value.ToString();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String[] words;
+            Int32 counter;
+            String devider = @"\s+|\n|\d+|\.|\,|\-";
+            byte[] wav_data = new byte[100000];
+            byte[] byte_data = new byte[4];
+
+            //-----------СТРОКИ ОПИСАНИЯ ОДНОГО СЛОВА ФАЙЛА БИБЛИОТЕКИ------------------------ 
+            String NameWord;
+            String LanthSamplWord;
+            String Sampl;
+            long CntRealSamples = 0;
+
+            long last_byte = 0;
+            long cnt_byte = 0;
+            long cnt_entr = 0;
+            long letter_shift = 0; // здвиг начала семплов слова относительно начала области семплов
+            long prev_word_pos = 0; // позиция начала слов на новую букву
+            int NoiseThreshold = (int)this.trackBar2.Value;
+            //-----------------------------------------------------------------------------
+            // однуляю словарь
+            /*
+            foreach (string key_ in DictAlphabet.Keys)
+            {
+                DictAlphabet[key_] = "Nan";
+            }
+            */
+            // devide text to string
+            words = Regex.Split(this.richTextBox1.Text, devider);
+            // delate empty string
+            words = words.Where(n => !string.IsNullOrEmpty(n)).ToArray();
+            if (words.Length==0)
+            {
+                MessageBox.Show(" Введите одно или более слов", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // запускаем диалог сохранения
+            this.saveFileDialog1.ShowDialog();
+            PATHSOUND = this.saveFileDialog1.SelectedPath + "\\" + @"Sound\";
+
+            //---------------------------СОЗДАЮ ДИРЕКТОРИЮ СЛОВАРЯ-------------------------------
+            System.IO.Directory.CreateDirectory(PATHSOUND + "\\" + WAVFOLDER);
+            //-------------------------------------------------------------------------------
+
+
+            // Sort string of text 
+            Array.Sort(words);
+            for (counter = 0; counter < words.Length; counter++)
+            {
+                words[counter] = words[counter].ToUpper();  //Все буквы заглавные
+            }
+            words = words.Distinct().ToArray();
+            // 
+            voice.Rate = this.trackBar1.Value; // Удаляю повторяющиеся слова
+            // 
+            voice.SelectVoice(this.comboBox1.SelectedItem.ToString());
+            // 
+            voice.Rate = this.trackBar1.Value;
+            voice.Volume = 100;
+            // Configure the audio output. 
+
+            for (counter = 0; counter < words.Length; counter++)
+            {
+                voice.SetOutputToWaveFile(PATHSOUND + WAVFOLDER + "\\" + words[counter] + ".wav", new SpeechAudioFormatInfo(16000, AudioBitsPerSample.Eight, AudioChannel.Mono));
+                voice.Speak(words[counter]);
+                voice.SetOutputToNull();
+            }
+
+            // -----------ГЕНЕРАЦИЯ БИБЛИОТЕКИ СЕМПЛОВ/СЛОВ------------------------------------------------
+            StreamWriter SampleFileStr, DicFileStr;  //Класс для записи в файл
+            FileInfo SampleFile = new FileInfo(PATHSOUND + SAMPLENANE);
+            SampleFileStr = SampleFile.AppendText(); //Дописываем инфу в файл, если файла не существует он создастся
+            FileInfo DicFile = new FileInfo(PATHSOUND + DICNANE);
+            DicFileStr = DicFile.AppendText(); //Дописываем инфу в файл, если файла не существует он создастся
+
+
+            // -------------------ФОРМИРУЮ ЗАПИСЬ СЛОВАРЯ---------------------------------------------
+            for (counter = 0; counter < words.Length; counter++)
+            {
+                //----------------КОПИРУЮ СЕМПЛЫ В СТРОКУ-----------------------------------------------
+                // Считываю весь WAV файл для обработки
+                FileStream samples_data = File.OpenRead(PATHSOUND + WAVFOLDER + "\\" + words[counter] + ".wav");
+                Array.Clear(wav_data, 0, 10000);
+                samples_data.Read(wav_data, 0, Convert.ToInt32(samples_data.Length));
+                // обнуляю строку семплов
+                Sampl = "";
+                // Ищу первый символ из конца с которого начинаються звук               
+                for (cnt_byte = samples_data.Length - 1; (wav_data[cnt_byte] > (128 - NoiseThreshold)) && (wav_data[cnt_byte] < (128 + NoiseThreshold)); cnt_byte--) { };
+                last_byte = cnt_byte;
+                // Ищу первый символ из начала с которого начинается звук 
+                for (cnt_byte = 48; (wav_data[cnt_byte] > (128 - NoiseThreshold)) && (wav_data[cnt_byte] < (128 + NoiseThreshold)); cnt_byte++) { };
+                CntRealSamples = last_byte - cnt_byte + 1;
+                if (CntRealSamples < 0) {
+                    MessageBox.Show("Слово "+ words[counter]+" не имеет звуковой дорожки", "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    SampleFileStr.Close(); // Закрываем файл семплов
+                    DicFileStr.Close();    // Закрываем файл словаря
+                    return;
+                }
+
+
+                // заполняю строку семплов
+                for (; cnt_byte <= last_byte; cnt_byte++)
+                {
+                    Sampl = Sampl + Convert.ToString(wav_data[cnt_byte]) + ",";
+                    // Добаляю символ переноса строки для каждого заданногоколичества символов
+                    cnt_entr++;
+                    if (cnt_entr >= 20) {
+                        cnt_entr = 0;
+                        Sampl = Sampl + "\r\n";
+                    }
+                }
+                //-----------------------------------------------------------------------------------------
+                //---------------------КОПИРУЮ РАЗМЕР ТРЕКА СЛОВА В СТРОКУ----------------------------------
+                byte_data = BitConverter.GetBytes((UInt32)CntRealSamples);
+                LanthSamplWord = byte_data[0].ToString() + "," + byte_data[1].ToString() + ",";
+                //------------------------------------------------------------------------------------------
+
+                // ---------------КОПИРУЮ ИМЯ ФАЙЛА ПОСИМВОЛЬНО В СТРОКУ-------------------------------
+                NameWord = "";
+              //  words[counter] = words[counter].ToUpper();  //Все буквы заглавные
+                foreach (char name_leter in words[counter])
+                    NameWord = NameWord + "'" + Convert.ToString(name_leter) + "',";//
+                NameWord = NameWord + "0,";
+                //--------------------------------------------------------------------------------------
+
+                //----------------------ЗАПИСЫВАЮ НОВОЕ СЛОВО В ФАЙЛ СЛОВАРЯ--------------------------------
+                SampleFileStr.Write(NameWord + LanthSamplWord + Sampl);
+                //------------------------------------------------------------------------------------------
+                // Заполнение словаря              
+                if (counter == 0) {
+                    DictAlphabet[Convert.ToString(words[counter][0])] =  letter_shift.ToString();
+                    letter_shift += 3 + words[counter].Length + CntRealSamples; // позиция слова относительно начала области семплов (ЗДВИГ)
+                    continue;
+                }
+                if (Convert.ToString(words[counter][0]) != Convert.ToString(words[counter - 1][0]))
+                {
+                    DictAlphabet[Convert.ToString(words[counter][0])] = letter_shift.ToString();
+                    DictAlphabet[Convert.ToString(words[counter-1][0])] =DictAlphabet[Convert.ToString(words[counter-1][0])] + ',' + (counter - prev_word_pos).ToString();
+                    prev_word_pos = counter;
+
+                }
+                letter_shift += 3 + words[counter].Length + CntRealSamples; // позиция слова относительно начала области семплов (ЗДВИГ)
+
+            }
+            // add last leter number words
+            DictAlphabet[Convert.ToString(words[counter - 1][0])] = DictAlphabet[Convert.ToString(words[counter - 1][0])] + ',' + (counter - prev_word_pos).ToString();
+            // fill dictionari file
+            foreach (string my_key in DictAlphabet.Keys) {
+                {
+                    DicFileStr.Write("{" + '\'' + my_key + '\'' + "," + DictAlphabet[my_key] + '}' + ",\r\n");
+            }
+
+        }
+
+             SampleFileStr.Close(); // Закрываем файл семплов
+             DicFileStr.Close();    // Закрываем файл словаря
+             MessageBox.Show("Added words: " + words.Length.ToString() + "\r\n" +
+                            "Size of dictionary, bytes: " + letter_shift.ToString() + "\r\n" +
+                            "Saved in: " + PATHSOUND,
+                            "Report",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+
+    }
+}
